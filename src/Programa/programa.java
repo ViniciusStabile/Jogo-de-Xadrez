e8fc5a9e52450c4cebb0c310584a6fbb93f6126e
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2023-03-16 10:46:33 -0300

    Jogo de xadrez, 60% ja concluido

---
+++ src/Programa/programa.java
@@ -0,0 +1,56 @@
+package Programa;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+import chess.ChessException;
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+public class programa{
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		ChessMatch chessMatch = new ChessMatch();
+		
+		List<ChessPiece> captured = new ArrayList<>();
+		
+		while (!chessMatch.getCheckMate()) {
+			try {
+				UI.clearScreen();
+				UI.printMatch(chessMatch,captured);
+				System.out.println();
+				System.out.print("Inicio: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				boolean[][] possibleMovies = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(),possibleMovies);
+				
+				System.out.println();
+				System.out.print("Destino: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				if ( capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
+				
+			}
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
+	}
+}
